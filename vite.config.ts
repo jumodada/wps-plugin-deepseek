e561083f7979278663e1e087db210a3b15e9bf51
wukunyun <864353360@qq.com>
2025-03-31 15:37:44 +0800

    feat: syns

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-
+import { copyFile } from "wpsjs/vite_plugins"
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   
   return {
     base: '/',
-    plugins: [react()],
+    plugins: [
+      copyFile({
+        src: 'manifest.xml',
+        dest: 'manifest.xml',
+      }),
+      react()
+    ],
     server: {
       host:'0.0.0.0',
       cors: true,
